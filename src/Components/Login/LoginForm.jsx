9ec16f9da3383820058fc27ced9d536e996fb8f3
Felipe <carlosfelipemonte1997@gmail.com>
2024-12-17 12:03:58 -0300

    Feat: create useForm hook

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../hooks/useForm';
 
 const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
   return (
+    // function handleSubmit(){
+
+    //   if(username.validate() && password.validate()){
+
+    //   }
+    // }
+
     <section>
       <h1>Login</h1>
       <form action="">
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
     </section>
